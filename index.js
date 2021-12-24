27245628e6f2fc6c9999c59ad71242c659b6bf4d
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-12-24 00:25:36 -0600

    additional work on logic

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const mysql = require('mysql2');
 // question set
 const questions = require('./lib/questions');
 // saved queries
-const queries = require('./lib/queries');
+const { showAll, addNew } = require('./lib/queries');
 
-const db = mysql.createConnection({
-    user: 'root',
-    database: 'employee_tracker_db'
-});
+// const db = mysql.createConnection({
+//     user: 'root',
+//     database: 'employee_tracker_db'
+// });
 
 const init = () => {    // ??
     // console.log(String.fromCharCode(2589, 2589, 2589, 2589));    // TODO ADD TITLE/ LOGO HERE
@@ -16,17 +16,21 @@ const init = () => {    // ??
 };
 
 const showMainMenu = () => {
+    
     inquirer.prompt(questions.mainMenuQuestions)
     .then(function (answers) {
         switch (answers.menuChoice) {
             case 'View Departments':
-                viewDepartments();
+                showAll("allDepts");
+                showMainMenu();
                 break;
             case 'View Roles':
-                ViewRoles();
+                showAll("allRoles");
+                showMainMenu();
                 break;
             case 'View Employees':
-                ViewEmployees();
+                showAll("allEmployees");
+                showMainMenu();
                 break;
             case 'Add New Department':
                 addDepartment();
@@ -46,42 +50,40 @@ const showMainMenu = () => {
             case 'Exit':
                 return process.exit();
         };
-        return init();
         });
-};
-
-
-const viewDepartments = () => {
-    console.table();
-};
-
-const ViewRoles = () => {
-
-};
-
-const ViewEmployees = () => {
-
 };
 
 const addDepartment = () => {
     inquirer.prompt(questions.addDepartmentQuestions)
     .then(function (answers) {
-
-    });
+        console.log(answer.deptName);
+        let obj = {"name": "Test Department"};
+        addNew("department", obj);
+    })
+    // .then(showMainMenu());
 };
 
 const addRole = () => {
+    // TODO query and return list of departments || ask for dept_id and offer prompt for list of vals
     inquirer.prompt(questions.addRoleQuestions)
     .then(function (answers) {
-
-    });
+        // TODO query to retrieve list of roles??
+        console.log(answers.roleTitle, answers.roleDept, answers.roleSalary);
+        let obj = {"title": "Test", "department_id": "7", "salary": "15000"};
+        addNew("role", obj);
+    })
+    // .then(showMainMenu());
 };
 
 const addEmployee = () => {
+    // todo query list of roles,  manager??
     inquirer.prompt(questions.addEmployeeQuestions)
     .then(function (answers) {
-
-    });
+        console.log(answers);
+        let obj = {"first_name": "Testy", "last_name": "Testerson", "role_id": "7", "manager_id": "NULL"};
+        addNew("employee", obj);
+    })
+    // .then(showMainMenu());
 };
 
 const updateEmployee = (fName, lName) => {
